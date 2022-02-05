627bf628c45a67042c31d3364be7bac34a703aad
bhargaviRdy <46837526+bhargaviRdy@users.noreply.github.com>
2022-02-06 01:28:33 +0530

    fix: amde responsive and corrected some hrefs

--- docs/sidebar.js
+++ docs/sidebar.js
@@ -3,12 +3,5 @@ const sidebarList = document.querySelector("#sidebar-list");
 const docs = document.querySelector(".docs");
 sidebarIcon.addEventListener("click", (event) => {
     event.preventDefault();
-    if(docs.style.marginLeft === "15%")
-        docs.style.marginLeft = "1rem";
-    else{
-     docs.style.marginLeft = "15%";
-    }
-    console.log(docs.style.marginLeft);
-
     sidebarList.classList.toggle("sidebar-active");
 })
